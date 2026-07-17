c7b5f44a2741b32f31208dbcc0e52c03fad7e0bd
Zac Chen <zacchen2024@gmail.com>
2026-07-17 02:12:40 +0000

    fix: 从 subtitles 公开入口导出 escapeAssText

--- src/subtitles/index.ts
+++ src/subtitles/index.ts
@@ -1,3 +1,3 @@
 export { segmentLines } from './segment.js'
-export { buildAss, buildKaraoke, formatAssTime } from './ass.js'
+export { buildAss, buildKaraoke, formatAssTime, escapeAssText } from './ass.js'
 export type { BuildAssOptions } from './ass.js'
